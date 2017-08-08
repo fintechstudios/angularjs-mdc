0329500f1d37c593e5cadcd16dce4898f39d3571
Dylan Praul <dylan.praul@gmail.com>
2017-08-08 12:21:58 -0400

    Try to use CircleCI workflows to deploy to GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // Used with webpack-dev-server
 const PUBLIC_PATH = '/assets/';
 const IS_DEV = process.env.MDC_ENV === 'development' || process.env.MDC_ENV === 'test';
+const IS_DEMOS = process.env.MDC_ENV === 'demos';
 const IS_TEST = process.env.MDC_ENV === 'test';
-const IS_PROD = process.env.MDC_ENV === 'production';
-const OUT_PATH = path.resolve('./dist');
+const IS_PROD = process.env.MDC_ENV === 'production' || process.env.MDC_ENV === 'demos';
+const OUT_PATH = IS_DEMOS ? path.resolve('./demos/assets') : path.resolve('./dist');
 const PACKAGE_NAME = 'angularjs-mdc';
 
 const CSS_LOADER_CONFIG = [
@@ -89,25 +90,28 @@ const CSS_EXPORT = {
 CSS_EXPORT['entry'][PACKAGE_NAME] = path.resolve('./src/mdc.scss');
 module.exports.push(CSS_EXPORT);
 
+const DEMO_CSS_EXPORT = {
+  name: 'demo-css',
+  entry: {
+    'demo-styles': path.resolve('./demos/demos.scss'),
+  },
+  output: {
+    path: OUT_PATH,
+    publicPath: PUBLIC_PATH,
+    filename: '[name].css.js',
+  },
+  devServer: DEV_SERVER_CONFIG,
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{loader: 'style-loader'}].concat(CSS_LOADER_CONFIG),
+    }],
+  },
+};
 
-if (IS_DEV) {
-  module.exports.push({
-    name: 'demo-css',
-    entry: {
-      'demo-styles': path.resolve('./demos/demos.scss'),
-    },
-    output: {
-      path: OUT_PATH,
-      publicPath: PUBLIC_PATH,
-      filename: '[name].css.js',
-    },
-    devServer: DEV_SERVER_CONFIG,
-    devtool: 'source-map',
-    module: {
-      rules: [{
-        test: /\.scss$/,
-        use: [{loader: 'style-loader'}].concat(CSS_LOADER_CONFIG),
-      }],
-    },
-  });
+if (IS_DEMOS) {
+  module.exports = [DEMO_CSS_EXPORT];
+} else if (IS_DEV) {
+  module.exports.push();
 }
